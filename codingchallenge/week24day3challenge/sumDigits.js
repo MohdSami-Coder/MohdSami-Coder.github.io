03268fd6f7266816202ac6680c13f41e2cfcc080
MohdSami-Coder <mhd.sami44@gmail.com>
2019-08-27 23:31:20 +0530

    sum of digits of number created by Sami

---
+++ codingchallenge/week24day3challenge/sumDigits.js
@@ -0,0 +1,12 @@
+function sum(n){
+
+    let result = 0;
+
+    while(n > 0){
+        result = result + (n % 10);
+        n = Math.floor(n / 10);
+    }
+    return result;
+}
+
+console.log( sum(Math.pow(2, 1000)) );
